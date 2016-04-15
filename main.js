7bf17974fc1a806b86001c55b71a7679b796b279
mirgags <markmiraglia@gmail.com>
2016-04-14 17:51:36 -0700

    got new tab working

--- main.js
+++ main.js
@@ -1,12 +1,19 @@
-function checkUrl(callback) {
-  var currentTab = chrome.tabs.getCurrent();
-  var curTabUrl = currentTab.url;
-  if(curTabUrl.length > 0) {
-    callback();
-  }
-  else {
-    break;
-  };
+var lastTabId = -1;
+
+function openTab() {
+  chrome.tabs.create({"url": "index.html",
+                      "active": true}, function(tab) {
+    chrome.tabs.update(tabs[0].id, {});
+  });
 };
-console.log("hi concole I'm an extension");
-chrome.tabs.create({"url": "./index.html"});
+
+function sendMessage() {
+  chrome.tabs.query({"active": true}, function(tabs) {
+    tabs[0].id;
+    chrome.tabs.sendMessage(lastTabId, "Background page started.");
+  });
+}
+
+chrome.browserAction.onClicked.addListener(function(tab) {
+  openTab();
+});

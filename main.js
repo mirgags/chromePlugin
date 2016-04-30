8ea356103cf802c9ade428c435a61e143986534e
mirgags <markmiraglia@gmail.com>
2016-04-29 17:29:21 -0700

    event pages

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 var lastTabId = -1;
 
-function openTab() {
+function openTab(callback) {
   chrome.tabs.create({"url": "index.html",
                       "active": true}, function(tab) {
     chrome.tabs.update(tabs[0].id, {});
@@ -12,8 +12,13 @@ function sendMessage() {
     tabs[0].id;
     chrome.tabs.sendMessage(lastTabId, "Background page started.");
   });
-}
+};
+
+function checkBathroom () {
+  $("#flexbox-3 > div:contains('Occupied')").css("color", "red");
+  $("#flexbox-3 > div:contains('Available')").css("color", "green");
+};
 
 chrome.browserAction.onClicked.addListener(function(tab) {
-  openTab();
+  openTab(funtion);
 });
